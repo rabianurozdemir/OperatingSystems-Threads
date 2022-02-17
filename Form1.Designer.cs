c4bcabdae94fab094438b4166f3bea8a766cabf6
Rabia Nur Özdemir <74839961+rabianurozdemir@users.noreply.github.com>
2022-02-18 00:17:43 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Odev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartPerformans = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.hesaplaButonu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPerformans)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartPerformans
+            // 
+            this.chartPerformans.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chartPerformans.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartPerformans.Legends.Add(legend1);
+            this.chartPerformans.Location = new System.Drawing.Point(46, 62);
+            this.chartPerformans.Name = "chartPerformans";
+            this.chartPerformans.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.LabelForeColor = System.Drawing.Color.Transparent;
+            series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.Red;
+            series1.MarkerSize = 8;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Series1";
+            this.chartPerformans.Series.Add(series1);
+            this.chartPerformans.Size = new System.Drawing.Size(764, 422);
+            this.chartPerformans.TabIndex = 0;
+            this.chartPerformans.Text = "chart1";
+            // 
+            // hesaplaButonu
+            // 
+            this.hesaplaButonu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.hesaplaButonu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hesaplaButonu.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hesaplaButonu.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.hesaplaButonu.Location = new System.Drawing.Point(332, 527);
+            this.hesaplaButonu.Name = "hesaplaButonu";
+            this.hesaplaButonu.Size = new System.Drawing.Size(141, 50);
+            this.hesaplaButonu.TabIndex = 1;
+            this.hesaplaButonu.Text = "Hesapla";
+            this.hesaplaButonu.UseVisualStyleBackColor = false;
+            this.hesaplaButonu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(936, 645);
+            this.Controls.Add(this.hesaplaButonu);
+            this.Controls.Add(this.chartPerformans);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartPerformans)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPerformans;
+        private System.Windows.Forms.Button hesaplaButonu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Odev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static decimal total = 0;
+
+        // standartToplama fonksiyonu normal toplama işlemi yapar.
+        // Burada baslangic ve bitis parametreleri daha sonradan thread'ler arasında iş bölümü yapmamız için gerekli
+        static decimal standartToplama(ulong baslangic, ulong bitis)
+        {
+            decimal toplam = 0;
+            for (ulong i = baslangic; i <= bitis; i++)
+            {
+                toplam += i;
+            }
+            //Console.WriteLine("Toplam = {0}", toplam);
+            total += toplam;
+            return toplam;
+        }
+
+        // gaussToplamı fonksiyonu toplama işlemini Gauss Yöntemi ile yapar.
+        static decimal gaussToplamı(decimal n)
+        {
+            decimal gaussToplamı = (n * (n + 1)) / 2;
+            Console.WriteLine("Gauss toplamı = {0}", gaussToplamı);
+            return gaussToplamı;
+        }
+
+        // threadCozumu fonksiyonu toplama işlemini 1-32 arası threadlere paylaştırarak yapar.
+        void threadCozumu(ulong threadSayisi)
+        {
+            for (ulong i = 1; i <= threadSayisi; i++)
+            {
+                Thread[] threadDizi = new Thread[i];
+
+                ulong parca = (Convert.ToUInt64(Math.Pow(10, 10))) / i; // Thread başına düşen toplanacak sayı adedi
+                ulong mod = ((Convert.ToUInt64(Math.Pow(10, 10))) % i); // Thread'lere iş bölümü yaptığımızda kalan toplanacak sayı adedi
+
+                Stopwatch stopwatch = new Stopwatch(); // Kodun çalıştığı süreyi bulmak için
+                stopwatch.Start();
+
+                Console.WriteLine("{0} adet thread için hesaplama başladı...", i);
+
+                for (ulong j = 0; j < i; j++)
+                {
+                    if (mod == 0) // 10^10 sayısı thread'lere tam bölünebiliyorsa
+                    {
+                        ulong baslangic = (parca * j) + 1;
+                        ulong bitis = parca * (j + 1);
+                        threadDizi[j] = new Thread(() => standartToplama(baslangic, bitis));
+                        threadDizi[j].Start();
+                    }
+                    else if (mod != 0) // 10^10 sayısı thread'lere tam bölünemiyorsa
+                    {
+                        ulong baslangic = (parca * j) + 1;
+                        ulong bitis = parca * (j + 1);
+
+                        if (i == (j + 1)) // Son thread'de isek kalan sayıları toplaması için son thread'e ver
+                        {
+                            threadDizi[j] = new Thread(() => standartToplama(baslangic, bitis + mod)); 
+                            threadDizi[j].Start();
+                        }
+                        else // Son thread'de değilsek normal devam et
+                        {
+                            threadDizi[j] = new Thread(() => standartToplama(baslangic, bitis));
+                            threadDizi[j].Start();
+                        }
+
+                    }
+
+                }
+
+                for (int k = 0; k < threadDizi.Length; k++) //
+                {
+                    while (threadDizi[k].IsAlive == true) ;
+
+                }
+
+                Console.WriteLine("Toplam: " + total);
+                total = 0;
+                stopwatch.Stop();
+                Console.WriteLine("Geçen süre: {0}", stopwatch.Elapsed.TotalMilliseconds);
+
+                chartPerformans.Series["Series1"].Points.Add(); // Grafik için gerekli verileri otomatik çekeceğiz
+                chartPerformans.Series["Series1"].Points[Convert.ToInt32(i - 1)].XValue = i; // x değeri
+                double[] yDegerleri = new double[1];
+                yDegerleri[0] = stopwatch.Elapsed.TotalMilliseconds;
+                chartPerformans.Series["Series1"].Points[Convert.ToInt32(i - 1)].YValues = yDegerleri; // y değeri
+                chartPerformans.Series["Series1"].Points[Convert.ToInt32(i - 1)].MarkerColor = Color.Red;
+                chartPerformans.Series["Series1"].Points[Convert.ToInt32(i - 1)].MarkerBorderColor = Color.Red;
+                chartPerformans.Series["Series1"].Points[Convert.ToInt32(i - 1)].MarkerSize = 5;
+                chartPerformans.Series["Series1"].Points[Convert.ToInt32(i - 1)].MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+                
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //standartToplama();
+            //gaussToplamı(Convert.ToDecimal(Math.Pow(10, 10)));
+            threadCozumu(32); // Hesapla butonuna tıklandığında grafik oluştur
+
+        }
+
+    }
+}
